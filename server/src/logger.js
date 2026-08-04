134d6115eb109a420cf53356e3e1f0b34567a9bc
KALANTZIS Ilias <ikalantz@netcompany.com>
2026-08-04 22:46:21 +0300

    chore: remove pino-pretty

--- server/src/logger.js
+++ server/src/logger.js
@@ -1,16 +1,9 @@
 import pino from "pino";
 
-// Set LOG_PRETTY=true in dev to enable colorized output
-const usePretty = process.env.LOG_PRETTY === "true";
-
 const logger = pino({
   level: process.env.LOG_LEVEL ?? "info",
-  ...(usePretty && {
-    transport: {
-      target: "pino-pretty",
-      options: { colorize: true, translateTime: "SYS:HH:MM:ss", ignore: "pid,hostname" }
-    }
-  })
 });
+
+export default logger;
 
 export default logger;
